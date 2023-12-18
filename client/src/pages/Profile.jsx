90a11ed491868cf878b3f017ee64f6ba60e5cb8d
janidu98 <janinduchathushan@gmail.com>
2023-12-19 00:59:25 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
+import { useRef } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 const Profile = () => {
 
@@ -9,52 +12,105 @@ const Profile = () => {
   // const [error, setError] = useState(null);
   const navigate = useNavigate();
   const {currentUser, error, loading} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePercentage, setFilePercentage] = useState(0);
+  const [fileUploadErr, setFileUploadErr] = useState(false);
+
+  useEffect(() => {
+    if(file){
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFilePercentage(Math.round(progress));
+    },
+      (error) => {
+        setFileUploadErr(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        })
+      }
+    )
+  }
 
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id] : e.target.value});
   }
 
-  const handleSubmit = async(e) => {
-    //prevent the refresh page
-    e.preventDefault();
+  // const handleSubmit = async(e) => {
+  //   //prevent the refresh page
+  //   e.preventDefault();
 
-    try {
-      setLoading(true);
+  //   try {
+  //     setLoading(true);
 
-      const config = {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      }
+  //     const config = {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-type': 'application/json',
+  //       },
+  //       body: JSON.stringify(formData),
+  //     }
 
-      const res = await fetch('/api/auth/signup', config);
-      const data = await res.json();
+  //     const res = await fetch('/api/auth/signup', config);
+  //     const data = await res.json();
 
-      if(data.success === false) {
-        setError(data.message);
-        setLoading(false);
-        return;
-      }
+  //     if(data.success === false) {
+  //       setError(data.message);
+  //       setLoading(false);
+  //       return;
+  //     }
 
-      setLoading(false);
-      setError(null);
-      navigate('/sign-in');
+  //     setLoading(false);
+  //     setError(null);
+  //     navigate('/sign-in');
 
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-    }
-  }
+  //   } catch (error) {
+  //     setError(error.message);
+  //     setLoading(false);
+  //   }
+  // }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
 
       <h1 className='text-center text-3xl font-semibold my-7'>Profile</h1>
 
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt='profile' className='rounded-full h-30 w-30 object-cover cursor-pointer self-center mt-2'/>
+      <form className='flex flex-col gap-4'>
+
+        <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*'/>
+        <img 
+          onClick={() => fileRef.current.click()} 
+          src={formData.avatar || currentUser.avatar} 
+          alt='profile' 
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+
+        <p className='text-sm self-center'>
+          {fileUploadErr ? (
+            <span className='text-red-700'>Error image upload(image must be less than 2 mb)</span>
+          ) : (
+            filePercentage > 0 && filePercentage < 100 ? (
+              <span className='text-slate-700'>{`Uploading ${filePercentage}%`}</span>
+            ) : (
+              filePercentage === 100 ? (
+                <span>Image successfully uploaded</span>
+              ) : ""
+            )
+          )}
+        </p>
+
         <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg' onChange={handleChange}/>
         <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg' onChange={handleChange}/>
         <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg' onChange={handleChange}/>
