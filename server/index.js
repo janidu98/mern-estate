8ef2b2ed7d71994704d33706b55b8cc564adcb46
janidu98 <janinduchathushan@gmail.com>
2023-12-22 21:18:18 +0530

    add pre installation

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/userRoute.js';
 import authRouter from './routes/authRoute.js';
 import listingRouter from './routes/listingRoute.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +24,12 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 //middleware
 app.use((err, req, res, next) => {
